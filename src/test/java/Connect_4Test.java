a37a4726bc458b565967b1c8b2b512084541a4d4
BettyES <betty.schirrmeister@gmail.com>
2017-06-25 17:56:18 +0100

    evaluateGameStatusDiagonal needs to be fixed

---
+++ src/test/java/Connect_4Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by glbes on 25/06/2017.
+ */
+class Connect_4Test {
+    @Test
+    void main() {
+
+    }
+
+}
